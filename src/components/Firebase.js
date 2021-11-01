2a120c32ea0c4017d2071f7b657869edc9ab3280
Adrian Deambroggi <62616924+DEAMBROGGI@users.noreply.github.com>
2021-11-01 02:30:51 -0300

    Update Firebase.js

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -3,14 +3,14 @@ import { getFirestore} from 'firebase/firestore/lite';
 import { getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzsCL7eQ37r7u_WHIWI9gN12LNt2q0qEY",
-  authDomain: "amdev-2e652.firebaseapp.com",
-  databaseURL: "https://amdev-2e652-default-rtdb.firebaseio.com",
-  projectId: "amdev-2e652",
-  storageBucket: "amdev-2e652.appspot.com",
-  messagingSenderId: "1004868076348",
-  appId: "1:1004868076348:web:e5f9932e44dc038289d439",
-  measurementId: "G-1DL648R1YD"
+  apiKey: "YOUR KEY",
+  authDomain: "YOUR KEY",
+  databaseURL: "YOUR KEY",
+  projectId: "YOUR KEY",
+  storageBucket: "YOUR KEY",
+  messagingSenderId: "YOUR KEY",
+  appId: "YOUR KEY",
+  measurementId: "YOUR KEY"
 };
 
 const app = initializeApp(firebaseConfig);
